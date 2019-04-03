fbe13c0065e62c74a890ce249653d0f90b04efdc
Jiwenjie <Jiwenjie97@gmail.com>
2019-04-03 11:54:36 +0800

    master commit desc: add customize(自定义) password or verification code(通用) and code optimization

--- app/src/main/java/com/example/root/projecttest/SplashActivity.java
+++ app/src/main/java/com/example/root/projecttest/SplashActivity.java
@@ -17,80 +17,85 @@ import com.example.root.projecttest.activity.*;
  */
 public class SplashActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        findViewById(R.id.btn1).setOnClickListener(this);
-        findViewById(R.id.btn2).setOnClickListener(this);
-        findViewById(R.id.btn3).setOnClickListener(this);
-        findViewById(R.id.btn4).setOnClickListener(this);
-        findViewById(R.id.btn5).setOnClickListener(this);
-        findViewById(R.id.btn6).setOnClickListener(this);
-        findViewById(R.id.btn7).setOnClickListener(this);
-        findViewById(R.id.btn8).setOnClickListener(this);
-        findViewById(R.id.btn9).setOnClickListener(this);
-        findViewById(R.id.btn10).setOnClickListener(this);
-        findViewById(R.id.btn11).setOnClickListener(this);
-        findViewById(R.id.btn12).setOnClickListener(this);
-        findViewById(R.id.btn13).setOnClickListener(this);
-    }
+   @Override
+   protected void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_splash);
+      findViewById(R.id.btn1).setOnClickListener(this);
+      findViewById(R.id.btn2).setOnClickListener(this);
+      findViewById(R.id.btn3).setOnClickListener(this);
+      findViewById(R.id.btn4).setOnClickListener(this);
+      findViewById(R.id.btn5).setOnClickListener(this);
+      findViewById(R.id.btn6).setOnClickListener(this);
+      findViewById(R.id.btn7).setOnClickListener(this);
+      findViewById(R.id.btn8).setOnClickListener(this);
+      findViewById(R.id.btn9).setOnClickListener(this);
+      findViewById(R.id.btn10).setOnClickListener(this);
+      findViewById(R.id.btn11).setOnClickListener(this);
+      findViewById(R.id.btn12).setOnClickListener(this);
+      findViewById(R.id.btn13).setOnClickListener(this);
+      findViewById(R.id.btn14).setOnClickListener(this);
+   }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn1: {
-                startActivity(new Intent(this, OnePictureProgressActivity.class));
-                break;
-            }
-            case R.id.btn2: {
-                startActivity(new Intent(this, MultiplePhotoSelectActivity.class));
-                break;
-            }
-            case R.id.btn3: {
-                startActivity(new Intent(this, MultipleGifSelectActivity.class));
-                break;
-            }
-            case R.id.btn4: {
-                startActivity(new Intent(this, TabLayoutMoveStopActivity.class));
-                break;
-            }
-            case R.id.btn5: {
-                startActivity(new Intent(this, CoordinatorLayoutActivity.class));
-                break;
-            }
-            case R.id.btn6: {
-                startActivity(new Intent(this, ExplorationAcoutCoordinatorLayoutActivity.class));
-                break;
-            }
-            case R.id.btn7: {
-                startActivity(new Intent(this, PopupWindowAnimationActivity.class));
-                break;
-            }
-            case R.id.btn8: {
-                startActivity(new Intent(this, DialogRvActivity.class));
-                break;
-            }
-            case R.id.btn9: {
-                startActivity(new Intent(this, DoubleClickActivity.class));
-                break;
-            }
-            case R.id.btn10: {
-                startActivity(new Intent(this, CameraActivity.class));
-               break;
-            }
-            case R.id.btn11: {
-                startActivity(new Intent(this, TestConnectImage.class));
-                break;
-            }
-           case R.id.btn12: {
-              startActivity(new Intent(this, ShareElementActivity.class));
-              break;
-           }
-            case R.id.btn13: {      // echars 图表的基本使用
-                startActivity(new Intent(this, ShareElementActivity.class));
-                break;
-            }
-        }
-    }
+   @Override
+   public void onClick(View v) {
+      switch (v.getId()) {
+         case R.id.btn1: {
+            startActivity(new Intent(this, OnePictureProgressActivity.class));
+            break;
+         }
+         case R.id.btn2: {
+            startActivity(new Intent(this, MultiplePhotoSelectActivity.class));
+            break;
+         }
+         case R.id.btn3: {
+            startActivity(new Intent(this, MultipleGifSelectActivity.class));
+            break;
+         }
+         case R.id.btn4: {
+            startActivity(new Intent(this, TabLayoutMoveStopActivity.class));
+            break;
+         }
+         case R.id.btn5: {
+            startActivity(new Intent(this, CoordinatorLayoutActivity.class));
+            break;
+         }
+         case R.id.btn6: {
+            startActivity(new Intent(this, ExplorationAcoutCoordinatorLayoutActivity.class));
+            break;
+         }
+         case R.id.btn7: {
+            startActivity(new Intent(this, PopupWindowAnimationActivity.class));
+            break;
+         }
+         case R.id.btn8: {
+            startActivity(new Intent(this, DialogRvActivity.class));
+            break;
+         }
+         case R.id.btn9: {
+            startActivity(new Intent(this, DoubleClickActivity.class));
+            break;
+         }
+         case R.id.btn10: {
+            startActivity(new Intent(this, CameraActivity.class));
+            break;
+         }
+         case R.id.btn11: {
+            startActivity(new Intent(this, TestConnectImage.class));
+            break;
+         }
+         case R.id.btn12: {
+            startActivity(new Intent(this, ShareElementActivity.class));
+            break;
+         }
+         case R.id.btn13: {      // echars 图表的基本使用
+            startActivity(new Intent(this, ShareElementActivity.class));
+            break;
+         }
+         case R.id.btn14: {      // echars 图表的基本使用
+            startActivity(new Intent(this, PassActivity.class));
+            break;
+         }
+      }
+   }
 }
---
+++ app/src/main/java/com/example/root/projecttest/activity/PassActivity.kt
@@ -0,0 +1,20 @@
+package com.example.root.projecttest.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.root.projecttest.R
+
+/**
+ *  author:Jiwenjie
+ *  email:278630464@qq.com
+ *  time:2019/04/03
+ *  desc:
+ *  version:1.0
+ */
+class PassActivity: AppCompatActivity() {
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_test_pass)
+   }
+}
